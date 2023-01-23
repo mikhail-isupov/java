55eda8c8b000d6ceffd543c9582e47548123f132
Mikhail Isupov <111003222+mikhail-isupov@users.noreply.github.com>
2023-01-23 17:18:43 +0700

    Delete OOP/abstract directory

--- OOP/abstract/GameMethods.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface GameMethods {
-    byte[] getInfo();
-    String getHeroType();
-    int getHealth();
-    void setHealth(byte damage);
-    void step(List<BaseHero> heroes);
-}
